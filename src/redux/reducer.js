82c87ec1e12a9a6033e714d0ebb920c88cbc7653
wen wang <2352473735@qq.com>
2020-04-02 14:45:35 +0800

    'finish'

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -6,7 +6,7 @@ import {SET_HEAD_TITLE,RECEIVE_USER,SHOW_ERROR_MSG,RESET_USER} from './action-ty
 /*
 用来管理头部标题的reducer函数
 */
-const initHeadTitle = '首页'
+const initHeadTitle = ''
 function headTitle(state = initHeadTitle, action){
     switch(action.type){
         case SET_HEAD_TITLE:
